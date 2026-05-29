5e6a14a7bdbd6a42e528866f8339963c2f715bff
TheinMinHtet <theinminhtet712005@gmail.com>
2026-05-29 11:09:10 +0630

    add dashboard

---
+++ prisma/migrations/20260529043856_add_onboarding_fields/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Shop" ADD COLUMN     "ageGroup" TEXT,
+ADD COLUMN     "businessCategory" TEXT[],
+ADD COLUMN     "businessGoal" TEXT,
+ADD COLUMN     "challenges" TEXT,
+ADD COLUMN     "marketingMethods" TEXT[],
+ADD COLUMN     "ownerName" TEXT,
+ADD COLUMN     "targetAudience" TEXT;
